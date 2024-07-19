5c48438b05c8fc901c214b03e366c25407e3793a
Marc-Flender <marc@famille-flender.fr>
2024-07-20 00:20:20 +0200

    modif index.html

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,51 +3,135 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+# données utilisateur: utilisé aussi dans nginx.conf
 chemin_utilisateurs='/home/yunohost.multimedia'
+
+# données issues de manifest.toml
+#   $domain
+#   $path=
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
 create_user () {
-ynh_print_info "Entrée dans create_user"
+#ynh_print_info "Entrée dans create_user"
 
 local user=$1
 local URL_webdav=$domain$path
 local page_contents=''
 
-$page_contents = <<EOPAGE
+# syntaxe here-doc: 
+#     EOPAGE est e delimiteur. 
+# commande en début de ligne: préférer cat à écho car echo ne travaille pas avec les pipe
+# redirection vers le fichier en première ligne du here-doc
+cat <<EOPAGE >   $chemin_utilisateurs/$user/index.html 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Webdav Yunohost</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
+    <meta charset="utf-8">
+    <base href="$domain/yunohost/sso/" target="_blank">
+    <title>My_Webdav Yunohost</title>
+
+  <!-- Responsive -->
+  <meta name="format-detection" content="telephone=no" />
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+
+  <!-- Do not index SSOWat pages -->
+  <meta name="robots" content="noindex, nofollow">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="assets/css/ynh_portal.css">
+  <link rel="stylesheet" href="assets/themes/default/custom_portal.css">
+
+  <!-- Icons -->
+  <link rel="shortcut icon" href="assets/icons/favicon.ico">
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/icons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/icons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/icons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/icons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/icons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/icons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/icons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/icons/apple-touch-icon-152x152.png">
+  <link rel="icon" type="image/png" href="assets/icons/favicon-196x196.png" sizes="196x196">
+  <link rel="icon" type="image/png" href="assets/icons/favicon-160x160.png" sizes="160x160">
+  <link rel="icon" type="image/png" href="assets/icons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="assets/icons/favicon-16x16.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="assets/icons/favicon-32x32.png" sizes="32x32">
+  <meta name="msapplication-TileColor" content="#41444f">
+  <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <script type="text/javascript" src="/ynh_portal.js"></script><link type="text/css" rel="stylesheet" href="/ynh_overlay.css"><script type="text/javascript" src="/ynhtheme/custom_portal.js"></script><link type="text/css" rel="stylesheet" href="/ynhtheme/custom_overlay.css"></head>
-<body>
-	<h1>Serveur Webdav sur Yunohost</h1>
-	<p>Si vous voyez cette page, c'est que la configuration du Webdav devrait fonctionner</p>
-    <p>Vous pouvez d&eacute;sormais vous connecter &agrave; votre serveur Webdav</p>
-    <p> URL : $URL_webdav</p>
-    <p>Utilisez votre nom d&rsquo;'utilisateur Yunohost et votre mot de passe pour vous authentifier</p>
-    <p>Vos donn&eacute;es seront stock&eacute;es dans le chemin $chemin_utilisateurs/$user</p>
-    
-<p>En cas de souci, rendez-vous sur 
-<a href="https://github.com/YunoHost-Apps/my_webdav_ynh">my_Webdav_ynh</a>.
-</p>
+<body class="ynh-user-portal logged">
+
+  <div id="ynh-logo" class="ynh-logo">
+    <span class="element-invisible">Yunohost</span>
+  </div>
+
+  <div class="content">
+<div class="ynh-wrapper user">
+  <ul class="user-menu">
+    <li><a id="ynh-logout" class="icon icon-connexion" href="?action=logout">Déconnexion</a></li>
+  </ul>
+
+  <a class="user-container user-container-info" href="edit.html">
+    <h2 class="user-username">marc</h2>
+    <small class="user-fullname">Marc Flender</small>
+    <span class="user-mail">marc@famille-flender.fr</span>
+  </a>
+
+</div>
+
+<div class="messages">
+
+	<h1>Serveur My_Webdav sur Yunohost
+    </h1>
+
+	<h2>Si vous voyez cette page, c'est que la configuration du Webdav devrait fonctionner
+    </h2>
+
+    <h3>Vous pouvez d&eacute;sormais vous connecter &agrave; votre serveur Webdav
+    </h3>
+    <p class="form-text"> URL : $URL_webdav</p>
+    <br>
+    <br>
+
+    <h3>Utilisez votre         <label class="control-label">Nom d&rsquo;utilisateur Yunohost </label>
+        <br>
+        et votre <label class="control-label">mot de passe Yunohost </label>pour vous authentifier
+    <br>
+    </h3>
+
+    <h3>Vos donn&eacute;es seront stock&eacute;es dans le chemin <label class="form-text">$chemin_utilisateurs/$user</label>
+    <br>
+    <br>
+    </h3>
+
+
+    <p>En cas de souci, rendez-vous sur 
+        <a href="https://github.com/YunoHost-Apps/my_webdav_ynh">my_Webdav_ynh</a>.
+    </p>
+
+
+  <div class="ynh-wrapper footer"><nav>
+    <a class="link-profile-edit" href="edit.html">Éditer mon profil</a>
+    <a class="link-documentation" href="//yunohost.org/docs" target="_blank">Documentation</a>
+    <a class="link-documentation" href="//yunohost.org/help" target="_blank">Support</a>
+    <a class="link-admin" href="/yunohost/admin/" target="_blank">Administration</a>
+  </nav></div>
+
+  </div>
+
+  <!-- Scripts -->
+  <script src="assets/js/ynh_portal.js"></script>
+  <script src="assets/themes/default/custom_portal.js"></script>
+
+
 </body>
 </html>
+
 EOPAGE
-
-echo $page_contents > $chemin_utilisateurs/$user/index.html
-
 }
-
 
 remove_user() {
 
